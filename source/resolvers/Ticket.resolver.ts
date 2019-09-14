3db81b99eba8361d256818603cd9be3f11bb3489
andela-oakinrele <oluwasinmisola.akinrele@andela.com>
2019-09-14 17:30:49 +0100

    sync data to db

--- source/resolvers/Ticket.resolver.ts
+++ source/resolvers/Ticket.resolver.ts
@@ -1,5 +1,7 @@
 import { Arg, Mutation, Query, Resolver } from "type-graphql"
 
+import axios from "axios"
+
 import TicketModel, { Ticket } from "../entities/ticket"
 
 import { AddTicketInput, ListTicketsInput, TicketInput } from "./types/Ticket.input"
@@ -23,6 +25,38 @@ export class TicketResolver {
       .sort((a, b) => b.date.getTime() - a.date.getTime())
       .slice(0, input.limit)
     return result
+  }
+
+  @Query(() => [Ticket])
+  public async fetchAndStoreCleanTickets(): Promise<Ticket[]> {
+    const cleanResponse = (results: any[]) => {
+      return results.map(result => {
+        let cleanResult = {...result, imageUrl: result.image, genre: result.genre && result.genre.split('|')}
+        delete(cleanResult.image)
+        delete(cleanResult._id)
+        return cleanResult
+      })
+    }
+
+    const queryAllData = async(skip = 0): Promise<any[]> => {
+      const limit = 100
+      const results: any = await axios.get(`https://us-central1-bonsai-interview-endpoints.cloudfunctions.net/movieTickets?skip=${skip}&limit=${limit}`)
+      if (results.data.length < 1) {
+        return cleanResponse(results.data)
+      } else {
+        skip += limit
+        return cleanResponse(results.data).concat(await queryAllData(skip))
+      }
+    }
+
+    const tickets = await queryAllData()
+    tickets.forEach(response => {
+      if (response.title && response.imageUrl) {
+        const ticket = new TicketModel(response)
+        return ticket.saveFields()
+      }
+    })
+    return tickets
   }
 
   @Mutation(() => Ticket)
